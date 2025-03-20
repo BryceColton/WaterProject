91ea57139c6fbdf6fb1d68769176068844ebe193
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-03-20 11:41:06 -0600

    frontend Phase01 nad backned Phase01 videos done

---
+++ backend/WaterProject/Controllers/WaterController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaterProject.Data;
+
+namespace WaterProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase
+    {
+
+        private WaterDbContext _waterContext;
+        public WaterController(WaterDbContext temp) => _waterContext = temp;
+
+        [HttpGet("AllProjects")]
+        public IEnumerable<Project> Get()
+        {
+            var something = _waterContext.Projects.ToList();
+
+            //return _waterContext.Projects.ToList();
+
+            return something;
+
+        }
+        [HttpGet("FunctionalProjects")]
+        public IEnumerable<Project> GetFunctionalProjects()
+        {
+            var something = _waterContext.Projects.Where(p => p.ProjectFunctionalityStatus == "Functional").ToList();
+            return something;
+        }
+    }
+
+    
+}
